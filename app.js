aa054814090fc0f093d5a6c0eb41128bc2af6186
brittytino <brittytino08@gmail.com>
2024-05-20 16:20:34 +0530

    new

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var cors = require('cors')
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const app = express();
 app.use(cors());
